55c086de3c18292d745a44584c707ba3e26d3cc0
ajaycheenath@gmail.com <ajaycheenath@gmail.com>
2020-10-14 14:33:23 +0200

    Auto config

--- src/main/java/com/cheenath/RestServiceApplication.java
+++ src/main/java/com/cheenath/RestServiceApplication.java
@@ -1,10 +1,12 @@
 package com.cheenath;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan(basePackages = "com.cheenath")
 public class RestServiceApplication {
 
