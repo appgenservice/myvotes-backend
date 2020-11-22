d1cdcd465b30c3535faaa3e82900408b867d8d33
your username <ajaycheenath@gmail.com>
2020-11-22 22:17:41 +0100

    App Manager add with extra params and deploy from REST

--- src/main/java/com/cheenath/service/AppService.java
+++ src/main/java/com/cheenath/service/AppService.java
@@ -32,7 +32,7 @@ public class AppService {
         AppDetails appDetails = appRepository.findById(appId).orElseThrow(() -> new AppManagementException(String.format("App id %d not found", appId)));
         String url = String.format(jenkinsURL, appDetails.getUrl(),appDetails.getBranch(), appDetails.getId(), appDetails.getAppName(), appDetails.getDbName(), appDetails.getDbUser(), appDetails.getDbPassword(), appDetails.getPort());
         HttpHeaders headers = new HttpHeaders(){{
-            String auth = "admin:1147548cbaaddcbb739f82ce2f0a59871e";
+            String auth = "admin:1145adeb8267296f334cae0d61549ada4e";
             byte[] encodedAuth = Base64.encodeBase64(
                     auth.getBytes(Charset.forName("US-ASCII")) );
             String authHeader = "Basic " + new String( encodedAuth );
