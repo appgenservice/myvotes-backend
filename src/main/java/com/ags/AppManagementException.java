26ee7ce13960fdef1dd0a8045f2745eee144783e
your username <ajaycheenath@gmail.com>
2020-11-28 18:30:34 +0100

    New java start up project

--- src/main/java/com/ags/AppManagementException.java
+++
@@ -1,7 +0,0 @@
-package com.ags;
-
-public class AppManagementException extends Exception {
-    public AppManagementException(final String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/ags/UserManagementException.java
@@ -0,0 +1,7 @@
+package com.ags;
+
+public class UserManagementException extends Exception {
+    public UserManagementException(final String message) {
+        super(message);
+    }
+}
