16ceda3faf8d7239c3a930cf77362954f2c3be59
your username <ajaycheenath@gmail.com>
2020-12-14 13:35:04 +0100

    Add poll and list polls functionalities

---
+++ src/main/java/com/ags/data/PollTopic.java
@@ -0,0 +1,96 @@
+package com.ags.data;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class PollTopic {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String question;
+    private String opinion1;
+    private String opinion2;
+    private String opinion3;
+    private String opinion4;
+    private String opinion5;
+    private Long createDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOpinion1() {
+        return opinion1;
+    }
+
+    public void setOpinion1(String opinion1) {
+        this.opinion1 = opinion1;
+    }
+
+    public String getOpinion2() {
+        return opinion2;
+    }
+
+    public void setOpinion2(String opinion2) {
+        this.opinion2 = opinion2;
+    }
+
+    public String getOpinion3() {
+        return opinion3;
+    }
+
+    public void setOpinion3(String opinion3) {
+        this.opinion3 = opinion3;
+    }
+
+    public String getOpinion4() {
+        return opinion4;
+    }
+
+    public void setOpinion4(String opinion4) {
+        this.opinion4 = opinion4;
+    }
+
+    public String getOpinion5() {
+        return opinion5;
+    }
+
+    public void setOpinion5(String opinion5) {
+        this.opinion5 = opinion5;
+    }
+
+    public Long getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Long createDate) {
+        this.createDate = createDate;
+    }
+
+
+}
